4e70675fa90be7df148b3b15facf5ddeb11018a5
NinoLS <belicnino77@gmail.com>
2021-01-12 14:19:48 +0100

    Modif : style and options

--- templates/Messages/index.php
+++ templates/Messages/index.php
@@ -6,15 +6,13 @@
  */
 ?>
 <div class="messages index content">
-    <?= $this->Html->link(__('Nouveau'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Messages') ?></h3>
+    <?= $this->Html->link(__('Nouveau'), ['action' => 'add'], ['class' => 'button float-left']) ?>
     <div class="table-responsive">
         <table>
             <thead>
                 <tr>
-                    <!-- <th><?= $this->Paginator->sort('id') ?></th> -->
                     <th><?= $this->Paginator->sort('De') ?></th>
-                    <!-- <th><?= $this->Paginator->sort('user_to') ?></th> -->
                     <th><?= $this->Paginator->sort('message') ?></th>
                     <th><?= $this->Paginator->sort('Date') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
@@ -23,14 +21,11 @@
             <tbody>
                 <?php foreach ($messages as $message) : ?>
                     <tr>
-                        <!-- <td><?= $this->Number->format($message->id) ?></td> -->
                         <td><?= h($message->user_from) ?></td>
-                        <!-- <td><?= h($message->user_to) ?></td> -->
                         <td><?= h($message->message) ?></td>
                         <td><?= h($message->created) ?></td>
                         <td class="actions">
                             <?= $this->Html->link(__('Voir'), ['action' => 'view', $message->id]) ?>
-                            <!-- <?= $this->Html->link(__('Edit'), ['action' => 'edit', $message->id]) ?> -->
                             <?= $this->Form->postLink(__('Supprimer'), ['action' => 'delete', $message->id], ['confirm' => __('Are you sure you want to delete # {0}?', $message->id)]) ?>
                         </td>
                     </tr>
@@ -38,7 +33,7 @@
             </tbody>
         </table>
     </div>
-    <div class="paginator">
+    <div class="paginator mt-5 text-danger">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('première')) ?>
             <?= $this->Paginator->prev('< ' . __('précédent')) ?>
--- templates/Messages/view.php
+++ templates/Messages/view.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Message $message
@@ -7,11 +8,8 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Message'), ['action' => 'edit', $message->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Message'), ['action' => 'delete', $message->id], ['confirm' => __('Are you sure you want to delete # {0}?', $message->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Messages'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Message'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+            <a class="button text-white" href="/chat/messages">Retour</a>
+            <?= $this->Form->postLink(__('Supprimer'), ['action' => 'delete', $message->id], ['confirm' => __('Are you sure you want to delete # {0}?', $message->id), 'class' => 'button text-white']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
@@ -19,23 +17,15 @@
             <h3><?= h($message->id) ?></h3>
             <table>
                 <tr>
-                    <th><?= __('User From') ?></th>
+                    <th><?= __('De') ?></th>
                     <td><?= h($message->user_from) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('User To') ?></th>
-                    <td><?= h($message->user_to) ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Message') ?></th>
                     <td><?= h($message->message) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($message->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
+                    <th><?= __('Date') ?></th>
                     <td><?= h($message->created) ?></td>
                 </tr>
             </table>
