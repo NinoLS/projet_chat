eef711bd3bb5637a5c5483d7652a2dee044c8d9b
NinoLS <belicnino77@gmail.com>
2021-01-16 13:53:37 +0100

    Add : redirect when /messages/add/... not a friend

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -51,40 +51,77 @@ class MessagesController extends AppController
         ];
         $user = $this->request->getSession()->read('Auth')->username;
 
-        $message = $this->Messages->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $message = $this->Messages->patchEntity($message, $this->request->getData());
-            $message['user_from'] = $user;
-            $message['user_to']   = $friend_with;
-            if ($this->Messages->save($message)) {
-                $this->Flash->success(__('Message envoyé à {0}.', ucfirst($message['user_to'])));
+        //users who added me
+        $users_added_me = $this->paginate($this->Messages->Friends
+            ->find()
+            ->where(['friend_with' => "$user"]));
+        $users_added_me = compact('users_added_me');
 
-                return $this->redirect(['action' => "add/$message->user_to"]);
+        //users who i added
+        $users_i_added = $this->paginate($this->Messages->Friends
+            ->find()
+            ->where(['username' => "$user"]));
+        $users_i_added = compact('users_i_added');
+
+        //get their names
+        $names_added_me = array();
+        $names_I_added = array();
+        foreach ($users_added_me as $users_tab) {
+            foreach ($users_tab as $user_tab) {
+                if (!empty($user_tab->username))
+                    array_push($names_added_me, $user_tab->username);
             }
-            $this->Flash->error(__("Message non envoyé: rééssayez ou contactez l'administrateur."));
+        }
+        foreach ($users_i_added as $users_tab) {
+            foreach ($users_tab as $user_tab) {
+                if (!empty($user_tab->friend_with))
+                    array_push($names_I_added, $user_tab->friend_with);
+            }
         }
 
-        $all_messages = $this->Messages->find()
-            ->where(
-                [
-                    'OR' =>
+        //MY FRIENDS CONVERSATIONS
+        $names_friends = array_intersect($names_I_added, $names_added_me);
+
+
+        if (in_array($friend_with, $names_friends)) {
+            $message = $this->Messages->newEmptyEntity();
+            if ($this->request->is('post')) {
+                $message = $this->Messages->patchEntity($message, $this->request->getData());
+                $message['user_from'] = $user;
+                $message['user_to']   = $friend_with;
+                if ($this->Messages->save($message)) {
+                    $this->Flash->success(__('Message envoyé à {0}.', ucfirst($message['user_to'])));
+
+                    return $this->redirect(['action' => "add/$message->user_to"]);
+                }
+                $this->Flash->error(__("Message non envoyé: rééssayez ou contactez l'administrateur."));
+            }
+
+            $all_messages = $this->Messages->find()
+                ->where(
                     [
+                        'OR' =>
                         [
-                            'user_from' => "$user",
-                            'user_to'   => "$friend_with"
-                        ],
-                        [
-                            'user_from' => "$friend_with",
-                            'user_to'   => "$user"
+                            [
+                                'user_from' => "$user",
+                                'user_to'   => "$friend_with"
+                            ],
+                            [
+                                'user_from' => "$friend_with",
+                                'user_to'   => "$user"
+                            ]
+
                         ]
-
                     ]
-                ]
-            )
-            ->order(['created' => 'ASC']);
-        $all_messages = $this->paginate($all_messages);
+                )
+                ->order(['created' => 'ASC']);
+            $all_messages = $this->paginate($all_messages);
 
-        $this->set(compact('all_messages', 'friend_with', 'message'));
+            $this->set(compact('all_messages', 'friend_with', 'message'));
+        } else {
+            $this->Flash->error(__('{0} ne fait pas partie des amis.', ucfirst($friend_with)));
+            $this->redirect('/chat');
+        }
     }
 
     public function delete($id = null)
