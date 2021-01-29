e3db831dff61807bc5dfab974d661eceb89c4114
NinoLS <belicnino77@gmail.com>
2021-01-29 21:56:28 +0100

    Add : js script, verif_username func

--- templates/Users/add.php
+++ templates/Users/add.php
@@ -9,13 +9,50 @@
             <h3><?= __('Nouvel utilisateur') ?></h3>
             <?= $this->Form->create($user) ?>
             <fieldset>
-                <?php
-                echo $this->Form->control('username', ['type' => 'text']);
-                echo $this->Form->control('password');
-                ?>
+                <?= $this->Form->control('username', ['type' => 'text']) ?>
+                <ul id="error_username" class="mb-5 d-none">
+                    <li>8-20 caractères</li>
+                    <li>Chiffres, lettres, tirets</li>
+                    <li>Pas d'espace</li>
+                </ul>
+                <?= $this->Form->control('password') ?>
+                <ul id="error_password" class="mb-5 d-none">
+                    <li>8-20 caractères</li>
+                    <li>Chiffres, lettres, tirets</li>
+                    <li>Pas d'espace</li>
+                </ul>
             </fieldset>
             <?= $this->Form->button(__('Créer'), ['class' => "bg-success border-0"]) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
 </div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script>
+    $(function() {
+        $("#username").blur(verif_username);
+        $("#password").blur(verif_password);
+    });
+
+    //VERIF GLOBALE : 
+    /* compte classe error (pas ouf)  
+    /* verif de tout <=> appel de toutes les fonctions
+    */
+
+    function verif_username() {
+        let username = $("#username").val().trim();
+        if (username.length < 8 || username.length > 20 || !username.match(/^[A-Za-z0-9_-]*$/)) {
+            $("#username").addClass("border-danger text-danger");
+            $("#error_username").removeClass("d-none");
+        } else {
+            $("#username").removeClass("border-danger text-danger");
+            $("#error_username").addClass("d-none");
+        }
+    }
+
+    function verif_password() {
+    }
+</script>
