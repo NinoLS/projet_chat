6c9acc625f4b35d06d7ae5df6d13ba0d350b6e5d
NinoLS <belicnino77@gmail.com>
2021-01-30 10:19:20 +0100

    Modif : sign in button instead of link + style (content)

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,14 +1,22 @@
-<div class="users form">
-    <?= $this->Flash->render() ?>
-    <h3>Connexion</h3>
-    <?= $this->Form->create() ?>
-    <fieldset>
-        <!-- <legend><?= __("Veuillez s'il vous plaît entrer votre nom d'utilisateur et votre mot de passe") ?></legend> -->
-        <?= $this->Form->control('username', ['required' => true]) ?>
-        <?= $this->Form->control('password', ['required' => true]) ?>
-    </fieldset>
-    <?= $this->Form->submit(__('Login'), ['class' => 'bg-success border-0']); ?>
-    <?= $this->Form->end() ?>
+<aside class="column">
+    <div class="side-nav">
+        <a href="/chat/users/add" class="button bg-info border-0 text-white">Sign in</a>
+    </div>
+</aside>
+<div class="row">
+    <div class="column-responsive column-80">
+        <div class="users form content">
+            <?= $this->Flash->render() ?>
+            <h3>Connexion</h3>
+            <?= $this->Form->create() ?>
+            <fieldset>
+                <!-- <legend><?= __("Veuillez s'il vous plaît entrer votre nom d'utilisateur et votre mot de passe") ?></legend> -->
+                <?= $this->Form->control('username', ['required' => true]) ?>
+                <?= $this->Form->control('password', ['required' => true]) ?>
+            </fieldset>
+            <?= $this->Form->submit(__('Login'), ['class' => 'bg-success border-0']); ?>
+            <?= $this->Form->end() ?>
 
-    <?= $this->Html->link("Ajouter un utilisateur", ['action' => 'add']) ?>
+        </div>
+    </div>
 </div>
