6436e7d5132675360ab350e79843821669edf2f0
NinoLS <belicnino77@gmail.com>
2021-01-30 18:17:45 +0100

    Add : confirm password field on user sign in

--- templates/Users/add.php
+++ templates/Users/add.php
@@ -16,13 +16,17 @@
                     <li>Pas d'espace</li>
                 </ul>
                 <?= $this->Form->control('password') ?>
-                <ul id="error_password" class="d-none">
+                <ul id="error_password" class="mb-5 d-none">
                     <li>8 caractères minimum</li>
                     <li>Une lettre minimum</li>
                     <li>Un chiffre minimum</li>
                 </ul>
+                <?= $this->Form->control('Confirm Password',['type' => 'password','id' => "confirm_password"]) ?>
+                <ul id="error_confirm_password" class="mb-5 d-none">
+                    <li>Le mot de passe ne correspond pas.</li>
+                </ul>
             </fieldset>
-            <?= $this->Form->button(__('Créer'), ['class' => "bg-success border-0", 'id' => "send_button"]) ?>
+            <?= $this->Form->button(__('Créer'), ['class' => "bg-info border-0", 'id' => "send_button"]) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
@@ -35,18 +39,25 @@
     $(function() {
         $("#username").blur(verif_username);
         $("#password").blur(verif_password);
+        $("#confirm_password").blur(verif_confirm_password);
+        
+        //si on a modifié les 2 champs
+        //action sur password => verif sur password & confirm_password
+        $("#password").blur(function(){
+            if($("#password").val().length > 0 && $("#confirm_password").val().length > 0)
+                verif_confirm_password();
+        });
 
+
+
+
+        
         $("#send_button").click(function() {
-            if (verif_username() & verif_password()) {
+            if (verif_username() & verif_password() & verif_confirm_password()) {
                 return true;
             } else return false;
         })
     });
-
-    //VERIF GLOBALE : 
-    /* compte classe text-danger (pas ouf)  
-    /* verif de tout <=> appel de toutes les fonctions
-    */
 
     function verif_username() {
         let username = $("#username").val().trim();
@@ -70,6 +81,20 @@
         } else {
             $("#password").removeClass("border-danger text-danger");
             $("#error_password").addClass("d-none");
+            return true;
+        }
+    }
+
+    function verif_confirm_password() {
+        let password = $("#password").val();
+        let confirm_password = $("#confirm_password").val();
+        if (confirm_password != password){
+            $("#confirm_password").addClass("border-danger text-danger");
+            $("#error_confirm_password").removeClass("d-none");
+            return false;
+        } else {
+            $("#confirm_password").removeClass("border-danger text-danger");
+            $("#error_confirm_password").addClass("d-none");
             return true;
         }
     }
