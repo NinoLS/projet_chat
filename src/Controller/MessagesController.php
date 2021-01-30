5b8890d4bc91a5daba7f244075b6dc2af2b71824
NinoLS <belicnino77@gmail.com>
2021-01-30 16:01:09 +0100

    Add : messages crypt system

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -1,10 +1,8 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace App\Controller;
-
-use Cake\Http\Session;
 
 class MessagesController extends AppController
 {
@@ -17,12 +15,12 @@ class MessagesController extends AppController
                     'OR' =>
                     [
                         [
-                            'user_to' => "$user"
+                            'user_to' => "$user",
                         ],
                         [
-                            'user_from' => "$user"
-                        ]
-                    ]
+                            'user_from' => "$user",
+                        ],
+                    ],
                 ]
             )
             ->order(['created' => 'DESC']);
@@ -61,20 +59,23 @@ class MessagesController extends AppController
         $names_I_added = array();
         foreach ($users_added_me as $users_tab) {
             foreach ($users_tab as $user_tab) {
-                if (!empty($user_tab->username))
+                if (!empty($user_tab->username)) {
                     array_push($names_added_me, $user_tab->username);
+                }
+
             }
         }
         foreach ($users_i_added as $users_tab) {
             foreach ($users_tab as $user_tab) {
-                if (!empty($user_tab->friend_with))
+                if (!empty($user_tab->friend_with)) {
                     array_push($names_I_added, $user_tab->friend_with);
+                }
+
             }
         }
 
         //MY FRIENDS CONVERSATIONS
         $names_friends = array_intersect($names_I_added, $names_added_me);
-
 
         //Vérification existence ami (au cas où //scripts injecteurs)
         if (in_array($friend_with, $names_friends)) {
@@ -82,51 +83,57 @@ class MessagesController extends AppController
             if ($this->request->is('post')) {
                 $message = $this->Messages->patchEntity($message, $this->request->getData());
                 $message['user_from'] = $user;
-                $message['user_to']   = $friend_with;
+                $message['user_to'] = $friend_with;
 
                 //1ere verification taille (avant modif longs mots)
-                if(empty($message->message) || strlen($message->message) >= 254)
-                {
+                if (empty($message->message) || strlen($message->message) >= 254) {
                     $this->Flash->error(__("Message non envoyé : taille inappropriée."));
                     return $this->redirect(['action' => "add/$message->user_to"]);
                 }
 
                 //vérification message espace " "
-                if($message->message == " ")
+                if ($message->message == " ") {
                     $message->message = "-";
+                }
 
                 //vérification long mot
                 define('MAX_SIZE_WORD', 12);
-                foreach(explode(" ",$message->message) as $word)
-                {
-                    if(strlen($word) > MAX_SIZE_WORD)
-                    {
+                foreach (explode(" ", $message->message) as $word) {
+                    if (strlen($word) > MAX_SIZE_WORD) {
                         //on coupe le mot par morceaux de taille MAX_SIZE_WORD
                         $word_to_words = array();
-                        for($i=0 ; $i < floor(strlen($word)/MAX_SIZE_WORD)+1 ; $i++)
-                            array_push($word_to_words,substr($word,MAX_SIZE_WORD * $i, MAX_SIZE_WORD));
-                        
+                        for ($i = 0; $i < floor(strlen($word) / MAX_SIZE_WORD) + 1; $i++) {
+                            array_push($word_to_words, substr($word, MAX_SIZE_WORD * $i, MAX_SIZE_WORD));
+                        }
+
                         //on remplit le tableau avec ces morceaux
                         $word_new = "";
-                        for($i=0 ; $i < floor(strlen($word)/MAX_SIZE_WORD) ; $i++)
-                            $word_new .= $word_to_words[$i]."-";
+                        for ($i = 0; $i < floor(strlen($word) / MAX_SIZE_WORD); $i++) {
+                            $word_new .= $word_to_words[$i] . "-";
+                        }
+
                         $word_new .= $word_to_words[$i];
 
                         //si mot initiale multiple de MAX_SIZE_WORD => un "-" en trop
-                        if(strlen($word) % MAX_SIZE_WORD == 0)
-                            $word_new[strlen($word_new)-1]=" ";
-                    
+                        if (strlen($word) % MAX_SIZE_WORD == 0) {
+                            $word_new[strlen($word_new) - 1] = " ";
+                        }
+
                         //on remplace le mot par le nouveau
-                        $message->message = str_replace($word,$word_new,$message->message);
+                        $message->message = str_replace($word, $word_new, $message->message);
                     }
                 }
-                
+
                 //2e verification taille (apres modif longs mots)
-                if(strlen($message->message) >= 254)
-                {
+                if (strlen($message->message) >= 254) {
                     $this->Flash->error(__("Message non envoyé : taille inappropriée."));
                     return $this->redirect(['action' => "add/$message->user_to"]);
                 }
+
+                //cryptage
+                $methods = openssl_get_cipher_methods();
+                $key = "toutaunefinsauflasaucissequienadeux01012021";
+                $message->message = openssl_encrypt($message->message, $methods[64], $key);
 
                 if ($this->Messages->save($message)) {
                     $this->Flash->success(__('Message envoyé à {0}.', ucfirst($message['user_to'])));
@@ -143,14 +150,14 @@ class MessagesController extends AppController
                         [
                             [
                                 'user_from' => "$user",
-                                'user_to'   => "$friend_with"
+                                'user_to' => "$friend_with",
                             ],
                             [
                                 'user_from' => "$friend_with",
-                                'user_to'   => "$user"
-                            ]
+                                'user_to' => "$user",
+                            ],
 
-                        ]
+                        ],
                     ]
                 )
                 ->order(['created' => 'ASC']);
@@ -160,6 +167,7 @@ class MessagesController extends AppController
             $this->Flash->error(__('{0} ne fait pas partie des amis.', ucfirst($friend_with)));
             $this->redirect('/chat');
         }
+
     }
 
     public function delete($id = null)
--- templates/Messages/add.php
+++ templates/Messages/add.php
@@ -1,3 +1,8 @@
+<?php 
+    $key = "toutaunefinsauflasaucissequienadeux01012021";
+    $methods = openssl_get_cipher_methods();
+?>
+
 <div class="row">
     <div class="messages index content col-10 mx-auto">
         <h3><?= __("Conversation avec 
@@ -11,11 +16,11 @@
                 echo '<div class="row mt-1">';
                 if ($all_messages->skip($i)->first()->user_from == $friend_with) {
                     echo '<div class="col-5">';
-                    echo '<p class="text-success table-success px-3 py-1 float-left"">' . $all_messages->skip($i)->first()->message . "</p>";
+                    echo '<p class="text-success table-success px-3 py-1 float-left"">' . openssl_decrypt($all_messages->skip($i)->first()->message,$methods[64],$key) . "</p>";
                     echo '</div><div class="col-5"></div>';
                 } else {
                     echo '<div class="col-5"></div><div class="col-5">';
-                    echo '<p class="text-dark table-secondary px-3 py-1 float-right">' . $all_messages->skip($i)->first()->message . "</p>";
+                    echo '<p class="text-dark table-secondary px-3 py-1 float-right">' . openssl_decrypt($all_messages->skip($i)->first()->message,$methods[64],$key) . "</p>";
                     echo '</div>';
                 }
                 echo '<div class="col-2">';
